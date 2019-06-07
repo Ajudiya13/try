aee4124021dc79328aa0dab12ecf7d67e4996424
Bhargav Ajudiya <ajudiya.bhargav@sprinklr.com>
2019-06-07 17:05:22 +0530

    third commit

---
+++ build-tracker-cli.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  applicationUrl: 'https://your-build-tracker-app', // The same as your server config `url`
+  artifacts: ['dist/**/*.js'] // an Array of glob-style file paths
+};
