bc54d82c9a2d74c3f88f7dabc53ecc15c7b3c999
Ajudiya13 <bhargavajudiya13299@gmail.com>
2019-06-07 18:22:56 +0530

    wow commit

--- build-tracker-cli.config.js
+++ build-tracker-cli.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  applicationUrl: '127.0.0.1:3000', // The same as your server config `url`
+  applicationUrl: 'http://localhost:3000', // The same as your server config `url`
   artifacts: ['dist/**/*.js'] // an Array of glob-style file paths
 };
--- build-tracker.config.js
+++ build-tracker.config.js
@@ -1,6 +1,6 @@
 const withPostgres = require('@build-tracker/plugin-with-postgres').default;
 module.exports = withPostgres({
-  url: '127.0.0.1:3000',
+  url: 'http://localhost:3000',
   pg: {
     user: 'myuser',
     password: 'mypassword',
