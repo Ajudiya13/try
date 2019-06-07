f21c93613c5146e54a31b92e227e4d7803368322
Ajudiya13 <bhargavajudiya13299@gmail.com>
2019-06-07 18:29:15 +0530

    updates config

--- build-tracker.config.js
+++ build-tracker.config.js
@@ -2,8 +2,8 @@ const withPostgres = require('@build-tracker/plugin-with-postgres').default;
 module.exports = withPostgres({
   url: 'http://localhost:3000',
   pg: {
-    user: 'myuser',
-    password: 'mypassword',
+    user: '',
+    password: '',
     host: '127.0.0.1',
     database: 'buildtracker'
   }
